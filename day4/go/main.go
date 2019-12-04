8bfeb4d53bff7fe3ec4b57d12327fb24ae971627
Nick Krichevsky <nick@ollien.com>
2019-12-04 00:55:40 -0500

    Add day 4 part 2 solution

--- day4/go/main.go
+++ day4/go/main.go
@@ -27,6 +27,38 @@ func isValidPart1Password(password int) bool {
 	return hasDoubleDigitPair
 }
 
+func isValidPart2Password(password int) bool {
+	// The part 2 rules are stricter than part 1; they must be a valid part 1 password
+	if !isValidPart1Password(password) {
+		return false
+	}
+
+	lastChar := rune(0)
+	charCount := 0
+	passwordStr := strconv.Itoa(password)
+	hasIsolatedRepeatedPair := false
+	for _, char := range passwordStr {
+		if lastChar == char {
+			charCount++
+		} else if hasIsolatedRepeatedPair {
+			// Once we find an isolated pair, the other digits don't matter
+			return true
+		} else {
+			charCount = 1
+		}
+
+		lastChar = char
+		if charCount > 2 {
+			// If the char count ever exceeds two, we need to say that we don't have an isolated pair
+			hasIsolatedRepeatedPair = false
+		} else if charCount == 2 {
+			hasIsolatedRepeatedPair = true
+		}
+	}
+
+	return hasIsolatedRepeatedPair
+}
+
 func main() {
 	if len(os.Args) != 3 {
 		fmt.Println("Usage: ./main lower_bound upper_bound")
@@ -42,12 +74,21 @@ func main() {
 		panic(fmt.Sprintf("could not parse upper bound: %s", err))
 	}
 
-	part1Passwords := 0
+	part1Passwords := []int{}
 	for password := lowerBound; password <= upperBound; password++ {
 		if isValidPart1Password(password) {
-			part1Passwords++
+			part1Passwords = append(part1Passwords, password)
 		}
 	}
 
-	fmt.Println(part1Passwords)
+	fmt.Println(len(part1Passwords))
+
+	numPart2Passwords := 0
+	for _, password := range part1Passwords {
+		if isValidPart2Password(password) {
+			numPart2Passwords++
+		}
+	}
+
+	fmt.Println(numPart2Passwords)
 }
